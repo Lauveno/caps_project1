3db94fef81937514491777133352edeba5d7f289
ayoung44 <a_young__@naver.com>
2020-05-12 16:28:31 +0900

    camera permission add

--- app/src/main/java/com/example/caps_project1/MypageActivity.java
+++ app/src/main/java/com/example/caps_project1/MypageActivity.java
@@ -1,12 +1,18 @@
 package com.example.caps_project1;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
@@ -15,6 +21,20 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MypageActivity extends Fragment {
+
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_IMAGE = 2;
+
+    private static final int my_permission_request = 2051;
+
+    private ImageView iv_profile;
+    private int id_view;
+
+
+
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,14 +48,7 @@ public class MypageActivity extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MypageActivity.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static MypageActivity newInstance(String param1, String param2) {
         MypageActivity fragment = new MypageActivity();
@@ -55,10 +68,36 @@ public class MypageActivity extends Fragment {
         }
     }
 
+    // onCreate : fragment가 생성될 때 호출되는 부분
+    // onCreateView : onCreate 후에 화면을 구성할 때 호출
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_mypage, container, false);
+
+        View view = inflater.inflate(R.layout.activity_mypage, container, false);
+
+        iv_profile = view.findViewById(R.id.iv_profile);
+        Button btn_uploadPicture = view.findViewById(R.id.btn_uploadPicture);
+        btn_uploadPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        return view;
+    }
+
+    // onActivityCreated : 프래그먼트 생성 후 호출하는 함수
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+
+        }
+
     }
 }
