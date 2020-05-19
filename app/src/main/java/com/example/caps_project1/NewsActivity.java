002196be23e0d15fc8eeff96d128005278e79b74
EunJin <julietjin94@gmail.com>
2020-05-20 06:11:42 +0900

    newsActiviy_stabilize

--- app/src/main/java/com/example/caps_project1/NewsActivity.java
+++ app/src/main/java/com/example/caps_project1/NewsActivity.java
@@ -1,13 +1,19 @@
 package com.example.caps_project1;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.AsyncTask;
+import android.widget.TextView;
+
 import org.jsoup.Jsoup;
 
 
@@ -20,7 +26,14 @@ public class NewsActivity extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-    private String htmlPageUrl = "http://www.naver.com/"; //파싱할 홈페이지의 URL주소
+    private String htmlPageUrl = "https://www.cabn.kr/news/section.html?sec_no=8";
+    //파싱할 홈페이지의 URL주소, 동물뉴스 웹페이지
+    private TextView textviewHtmlDocument;  //추가_
+    private String htmlContentInStringFormat=""; //추가_
+    private FragmentActivity mContext;
+    private Context context;
+    private RecyclerAdapter adapter;
+    RecyclerView recyclerView;
 
 
     // TODO: Rename and change types of parameters
@@ -47,6 +60,8 @@ public class NewsActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_news, container, false);
+        View view =  inflater.inflate(R.layout.activity_news,container,false);
+
+        return view;
     }
 }
