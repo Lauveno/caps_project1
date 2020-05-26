bb89ecfc5df543f0c25a454973ff90f5dd4942ba
EunJin <julietjin94@gmail.com>
2020-05-27 05:24:50 +0900

    get newsheadline from navernews

--- app/src/main/java/com/example/caps_project1/Data.java
+++ app/src/main/java/com/example/caps_project1/Data.java
@@ -2,9 +2,17 @@ package com.example.caps_project1;
 
 public class Data {
     //news data를 위한 class
-    private String title;
-    private String content;
-    //private  news;
+    private String title; //id : textView_title
+    private String content; //id : textView_content (url)
+
+    public Data(){
+
+    }
+
+    public Data(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 
     public String getTitle() {
         return title;
@@ -22,7 +30,4 @@ public class Data {
         this.content = content;
     }
 
-    //public String getNews() {   return news;    }
-
-    //public void setNews(String date) {this.news = news;    }
 }
