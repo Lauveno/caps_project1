e19368d93c21bab4f62c15bc3fc25c6b313eb0e6
EunJin <julietjin94@gmail.com>
2020-06-07 00:41:32 +0900

    design changed

--- app/src/main/java/com/example/caps_project1/Board.java
+++ app/src/main/java/com/example/caps_project1/Board.java
@@ -13,7 +13,7 @@ public class Board {
 
     public String getTitle(){return title;}
     public String getContent(){return content;}
-
-    
+    public void setTitle(String title){this.title = title;}
+    public void setContent(String content){this.title = content;}
 
 }
